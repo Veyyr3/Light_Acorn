5b91f4a683bafb7363d4af5502250fb0f4317875
Veyyr <dimapolska967@gmail.com>
2026-03-05 22:18:03 +0500

    feat: update acron game tools

--- src/acorn_kernel/acorn_tools/acorn_game_tools/acorn_game_tools_heart.rs
+++ src/acorn_kernel/acorn_tools/acorn_game_tools/acorn_game_tools_heart.rs
@@ -1,13 +1,7 @@
 // src/acorn_kernel/acorn_tools/acorn_game_tools/acorn_game_tools_heart.rs
-
-/// It is your 3D model.
-/// Rules of Macroquad: you must use u16 for indices (it's strict).
-pub struct AcornMeshAsset {
-    pub vertices: Vec<f32>, // How many dots in your 3D model. Hold on... What? Imagine: 0.6 dots in your triangle.
-    pub indices: Vec<u16> // You say to GPU: how link your dots by pack with 3 vertices. Every vertices have own number (from 0 to 65535). 0 -> 1 -> 2 and you have triangle!
-}
+use macroquad::models::Mesh;
 
 /// It is vector of your 3D models.
-pub struct AssetDatabase {
-    pub meshes: Vec<AcornMeshAsset>
+pub struct Acorn3DAssetDatabase {
+    pub meshes: Vec<Mesh>
 }
