3bf21aaacaae890d1c4a556ea57c9974cb7c86d6
Veyyr <dimapolska967@gmail.com>
2026-03-07 13:15:18 +0500

    chore: add licenses to files

--- src/acorn_kernel/acorn_settings.rs
+++ src/acorn_kernel/acorn_settings.rs
@@ -1,3 +1,7 @@
+// (c) 2026 Lord of the Light Acorn: Veyyr3.
+// This file is part of Light Acorn and is distributed under the MIT License.
+// See the LICENSES folder in the project root for the full license text.
+
 // src/acorn_kernel/acorn_heart.rs
 use crate::acorn_kernel::{
     acorn_heart::Zone, 
