3f3c3221352cdcbfe949ec2129c80d9056710c4c
Veyyr <dimapolska967@gmail.com>
2026-03-21 15:31:33 +0500

    chore: add REACORN to templates

---
+++ TEMPLATES/REACORN_WAY/default/src/acorn_settings.rs
@@ -0,0 +1,29 @@
+// (c) 2026 Lord of the Light Acorn: Veyyr3.
+// This file is part of Light Acorn and is distributed under the MIT License.
+// See the LICENSES folder in the project root for the full license text.
+
+/*
+Warning: YOU shouldn't delete this file because other Acorn files use AcornZoneContext 
+
+Warning: Do not rename 'before_2d_zone' or 'after_2d_zone' fields, kernel depends on them
+*/
+
+// src/acorn_settings.rs
+use crate::acorn_kernel::{
+    acorn_heart::Zone, 
+};
+// game suggestions
+use crate::acorn_tools::acorn_game_tools::agt_heart::Acorn3DAssetDatabase;
+
+/// Contain here your Zones
+pub struct AcornZoneContext {
+    pub before_2d_zone: Zone,
+    pub after_2d_zone: Zone,
+    // add here your Zone trough comma
+}
+
+/// Contain here your global statements 
+pub struct AcornGlobalContext {
+    // from game tools
+    pub assets_3d: Acorn3DAssetDatabase,
+}
---
+++ TEMPLATES/REACORN_WAY/default/src/main.rs
@@ -0,0 +1,68 @@
+// (c) 2026 Lord of the Light Acorn: Veyyr3.
+// This file is part of Light Acorn and is distributed under the MIT License.
+// See the LICENSES folder in the project root for the full license text.
+
+// src/main.rs
+mod acorn_kernel;
+mod acorn_zsetup; // zone setup
+mod acorn_gsetup; // global setup
+mod acorn_settings;
+// for game
+mod acorn_tools;
+
+use acorn_kernel::{
+    acorn_render::acorn_loop, // import acorn_loop
+    acorn_heart::AcornECS, // import Zone, Location, AcornECS
+};
+use acorn_zsetup::{ // import functions from acorn_setup for use in Main
+    acorn_zone_setup,
+    // other example functions
+    acorn_game_spawn_acorn,  
+    acorn_example_spawn_entity
+};
+use acorn_gsetup::acorn_global_setup;
+
+/*
+Hi!
+
+This main.rs file.
+
+Examples are in acorn_setup.rs which you may try and search.
+
+======================
+Right now you are using tempelate REACORN-way (when you can reoder functions in runtime).
+BUT IF YOU DON'T WANT MUTABLE CODE IN RUNTIME: use ACORN WAY template in "TEMPLATES" folder.
+======================
+
+See other templates of projects in "TEMPLATES" folder.
+
+======================
+Memorise: Zone is when, Location is where, Function is time-marker.
+======================
+*/
+
+#[macroquad::main("Light Acorn test")]
+async fn main() {
+    // Global variable ECS. Hand over to acorn_loop.
+    let mut acorn_ecs = AcornECS::default();
+
+    // Contex of Zones. Hand over to acorn_loop.
+    let mut acorn_zone_context = acorn_zone_setup();
+    // Global states. Hand over to acorn_loop.
+    let mut acorn_global_context = acorn_global_setup();
+
+    // Create entities here (or in runtime by your logic)
+    acorn_example_spawn_entity(
+        &mut acorn_ecs.world, 
+        &mut acorn_zone_context, 
+        &mut acorn_global_context
+    );
+    acorn_game_spawn_acorn(
+        &mut acorn_ecs.world, 
+        &mut acorn_zone_context, 
+        &mut acorn_global_context
+    );
+
+    // main loop
+    acorn_loop(acorn_ecs, acorn_zone_context, acorn_global_context).await;
+}
