c46e75de0fcfd3f7f4ae1db725fe7617bc14ce65
Veyyr <dimapolska967@gmail.com>
2026-06-14 14:54:59 +0500

    ref: replace camera to normal

--- src/acorn_zsetup.rs
+++ src/acorn_zsetup.rs
@@ -74,7 +74,7 @@ pub fn acorn_zone_setup() -> AcornZoneContext {
         },
         // Location for UI input
         location! {
-            agt_camera_3d_control_fps_collision_experimental, // update camera position and look
+            agt_camera_3d_control_fps, // update camera position and look
         }
         // add own locations through comma
     };
