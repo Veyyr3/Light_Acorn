420caa72b6be806488c75c07570034567ff6211b
Veyyr <dimapolska967@gmail.com>
2026-07-09 20:00:40 +0500

    feat: add new 3D model

--- src/acorn_gsetup.rs
+++ src/acorn_gsetup.rs
@@ -57,9 +57,10 @@ pub fn acorn_global_setup() -> AcornGlobalContext {
     // PLEASE, remember index of your 3d models when you add news.
     // It so, because for perfomance. 
     // BUT I leave it to you for organize logic assets keeping.
-    assets_3d.meshes.push(
-        load_obj_with_materials_to_mesh("objs/acorn_engine.obj")
-    );
+    assets_3d.meshes.extend([
+        load_obj_with_materials_to_mesh("objs/acorn_engine.obj"),
+        load_obj_with_materials_to_mesh("objs/yellow_platform.obj"),
+    ]);
     // =================================
     
 
